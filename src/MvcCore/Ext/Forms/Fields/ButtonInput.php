1839af8a273728e27b969cf1ffec347327741962
Tom Flídr <tomflidr@gmail.com>
2018-09-14 15:29:44 +0200

    creating validator with all options in constructor

---
+++ src/MvcCore/Ext/Forms/Fields/ButtonInput.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * MvcCore
+ *
+ * This source file is subject to the BSD 3 License
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that are distributed with this source code.
+ *
+ * @copyright	Copyright (c) 2016 Tom Flídr (https://github.com/mvccore/mvccore)
+ * @license		https://mvccore.github.io/docs/mvccore/4.0.0/LICENCE.md
+ */
+
+namespace MvcCore\Ext\Forms\Fields;
+
+/**
+ * Responsibility: init, predispatch and render button based 
+ *				   on `<input type="button">` HTML element.
+ *				   Button has text `OK` by default and no validators.
+ */
+class		ButtonInput 
+extends		\MvcCore\Ext\Forms\Field 
+implements	\MvcCore\Ext\Forms\Fields\IVisibleField
+{
+	use \MvcCore\Ext\Forms\Field\Props\VisibleField;
+
+	/**
+	 * Possible values: `button`.
+	 * @var string
+	 */
+	protected $type = 'button';
+	
+	/**
+	 * Default visible button text - `OK`.
+	 * This button text is automaticly checked, if there is at least any 
+	 * visible text and automaticly translated, if any translator `callable` 
+	 * defined in form instance.
+	 * @var string
+	 */
+	protected $value = 'OK';
+	
+	/**
+	 * This INTERNAL method is called from `\MvcCore\Ext\Form` after field
+	 * is added into form instance by `$form->AddField();` method. Do not 
+	 * use this method even if you don't develop any form field.
+	 * - Check if field has any name, which is required.
+	 * - Set up form and field id attribute by form id and field name.
+	 * - Set up required.
+	 * - Set up translate boolean property.
+	 * - Check if exists button `value` string.
+	 * - Translate button value if necessary.
+	 * @param \MvcCore\Ext\Form|\MvcCore\Ext\Forms\IForm $form
+	 * @throws \InvalidArgumentException
+	 * @return \MvcCore\Ext\Forms\Fields\SubmitInput|\MvcCore\Ext\Forms\IField
+	 */
+	public function & SetForm (\MvcCore\Ext\Forms\IForm & $form) {
+		parent::SetForm($form);
+		if (!$this->value) $this->throwNewInvalidArgumentException(
+			'No button `value` defined.'
+		);
+		return $this;
+	}
+	
+	/**
+	 * This INTERNAL method is called from `\MvcCore\Ext\Form` just before
+	 * field is naturally rendered. It sets up field for rendering process.
+	 * Do not use this method even if you don't develop any form field.
+	 * - Set up field render mode if not defined.
+	 * - Translate label text if necessary.
+	 * - Translate value text if necessary.
+	 * - Set up tabindex if necessary.
+	 * @return void
+	 */
+	public function PreDispatch () {
+		parent::PreDispatch();
+		if ($this->translate && $this->value)
+			$this->value = $this->form->Translate($this->value);
+		$this->preDispatchTabIndex();
+	}
+}
