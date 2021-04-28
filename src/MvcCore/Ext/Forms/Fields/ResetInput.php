9580a7ad7d7e76a33132641047b3b4ccdadc4629
Tom Flídr <tomflidr@gmail.com>
2021-04-28 23:21:55 +0200

    emoved useless supporting JS script, rich constructor for named params in PHP8+, `IReset` interface for reset button and reset input.

--- src/MvcCore/Ext/Forms/Fields/ResetInput.php
+++ src/MvcCore/Ext/Forms/Fields/ResetInput.php
@@ -21,7 +21,8 @@ namespace MvcCore\Ext\Forms\Fields;
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
 class		ResetInput 
 extends		\MvcCore\Ext\Forms\Field
-implements	\MvcCore\Ext\Forms\Fields\IVisibleField {
+implements	\MvcCore\Ext\Forms\Fields\IReset,
+			\MvcCore\Ext\Forms\Fields\IVisibleField {
 
 	use \MvcCore\Ext\Forms\Field\Props\VisibleField;
 
@@ -41,40 +42,6 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField {
 	protected $value = 'Reset';
 
 	/**
-	 * Supporting javascript full javascript class name.
-	 * If you want to use any custom supporting javascript prototyped class
-	 * for any additional purposes for your custom field, you need to use
-	 * `$field->jsSupportingFile` property to define path to your javascript file
-	 * relatively from configured `\MvcCore\Ext\Form::SetJsSupportFilesRootDir(...);`
-	 * value. Than you have to add supporting javascript file path into field form 
-	 * in `$field->PreDispatch();` method to render those files immediately after form
-	 * (once) or by any external custom assets renderer configured by:
-	 * `$form->SetJsSupportFilesRenderer(...);` method.
-	 * Or you can add your custom supporting javascript files into response by your 
-	 * own and also you can run your helper javascripts also by your own. Is up to you.
-	 * `NULL` by default.
-	 * @var string
-	 */
-	protected $jsClassName = 'MvcCoreForm.Reset';
-
-	/**
-	 * Field supporting javascript file relative path.
-	 * If you want to use any custom supporting javascript file (with prototyped 
-	 * class) for any additional purposes for your custom field, you need to 
-	 * define path to your javascript file relatively from configured 
-	 * `\MvcCore\Ext\Form::SetJsSupportFilesRootDir(...);` value. 
-	 * Than you have to add supporting javascript file path into field form 
-	 * in `$field->PreDispatch();` method to render those files immediately after form
-	 * (once) or by any external custom assets renderer configured by:
-	 * `$form->SetJsSupportFilesRenderer(...);` method.
-	 * Or you can add your custom supporting javascript files into response by your 
-	 * own and also you can run your helper javascripts also by your own. Is up to you.
-	 * `NULL` by default.
-	 * @var string
-	 */
-	protected $jsSupportingFile = \MvcCore\Ext\IForm::FORM_ASSETS_DIR_REPLACEMENT . '/fields/reset.js';
-	
-	/**
 	 * Standard field control natural template string.
 	 * @var \string[]|\stdClass
 	 */
@@ -84,13 +51,101 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField {
 
 	/**
 	 * Create new form `<input type="reset" />` control instance.
-	 * @param  array $cfg Config array with public properties and it's 
-	 *                    values which you want to configure, presented 
-	 *                    in camel case properties names syntax.
+	 * 
+	 * @param  array      $cfg
+	 * Config array with public properties and it's
+	 * values which you want to configure, presented
+	 * in camel case properties names syntax.
+	 * 
+	 * @param  string     $name 
+	 * Form field specific name, used to identify submitted value.
+	 * This value is required for all form fields.
+	 * @param  string     $type 
+	 * Form field type, used in `<input type="...">` attribute value.
+	 * Every typed field has it's own string value, but base field type 
+	 * `\MvcCore\Ext\Forms\Field` has `NULL`.
+	 * @param  string     $value 
+	 * Form field value. It could be string or array, int or float, it depends
+	 * on field implementation. Default value is `NULL`.
+	 * @param  string     $title 
+	 * Field title, global HTML attribute, optional.
+	 * @param  string     $translate 
+	 * Boolean flag about field visible texts and error messages translation.
+	 * This flag is automatically assigned from `$field->form->GetTranslate();` 
+	 * flag in `$field->Init();` method.
+	 * @param  string     $translateTitle 
+	 * Boolean to translate title text, `TRUE` by default.
+	 * @param  array      $cssClasses 
+	 * Form field HTML element css classes strings.
+	 * Default value is an empty array to not render HTML `class` attribute.
+	 * @param  array      $controlAttrs 
+	 * Collection with field HTML element additional attributes by array keys/values.
+	 * Do not use system attributes as: `id`, `name`, `value`, `readonly`, `disabled`, `class` ...
+	 * Those attributes has it's own configurable properties by setter methods or by constructor config array.
+	 * HTML field elements are meant: `<input>, <button>, <select>, <textarea> ...`. 
+	 * Default value is an empty array to not render any additional attributes.
+	 * 
+	 * @param  string     $accessKey
+	 * The access key global attribute provides a hint for generating
+	 * a keyboard shortcut for the current element. The attribute 
+	 * value must consist of a single printable character (which 
+	 * includes accented and other characters that can be generated 
+	 * by the keyboard).
+	 * @param  bool       $autoFocus
+	 * This Boolean attribute lets you specify that a form control should have input
+	 * focus when the page loads. Only one form-associated element in a document can
+	 * have this attribute specified. 
+	 * @param  bool       $disabled
+	 * Form field attribute `disabled`, determination if field value will be 
+	 * possible to change by user and if user will be graphically informed about it 
+	 * by default browser behaviour or not. Default value is `FALSE`. 
+	 * This flag is also used for sure for submit checking. But if any field is 
+	 * marked as disabled, browsers always don't send any value under this field name
+	 * in submit. If field is configured as disabled, no value sent under field name 
+	 * from user will be accepted in submit process and value for this field will 
+	 * be used by server side form initialization. 
+	 * Disabled attribute has more power than required. If disabled is true and
+	 * required is true and if there is no or invalid submitted value, there is no 
+	 * required error and it's used value from server side assigned by 
+	 * `$form->SetValues();` or from session.
+	 * @param  int|string $tabIndex
+	 * An integer attribute indicating if the element can take input focus (is focusable), 
+	 * if it should participate to sequential keyboard navigation, and if so, at what 
+	 * position. You can set `auto` string value to get next form tab-index value automatically. 
+	 * Tab-index for every field in form is better to index from value `1` or automatically and 
+	 * moved to specific higher value by place, where is form currently rendered by form 
+	 * instance method `$form->SetBaseTabIndex()` to move tab-index for each field into 
+	 * final values. Tab-index can takes several values:
+	 * - a negative value means that the element should be focusable, but should not be 
+	 *   reachable via sequential keyboard navigation;
+	 * - 0 means that the element should be focusable and reachable via sequential 
+	 *   keyboard navigation, but its relative order is defined by the platform convention;
+	 * - a positive value means that the element should be focusable and reachable via 
+	 *   sequential keyboard navigation; the order in which the elements are focused is 
+	 *   the increasing value of the tab-index. If several elements share the same tab-index, 
+	 *   their relative order follows their relative positions in the document.
+	 * 
 	 * @throws \InvalidArgumentException
 	 * @return \MvcCore\Ext\Forms\Fields\ButtonInput
 	 */
-	public function __construct (array $cfg = []) {
+	public function __construct (
+		array $cfg = [], 
+
+		$name = NULL, 
+		$type = NULL, 
+		$value = NULL, 
+		$title = NULL, 
+		$translate = NULL, 
+		$translateTitle = NULL, 
+		array $cssClasses = [], 
+		array $controlAttrs = [], 
+
+		$accessKey = NULL,
+		$autoFocus = NULL,
+		$disabled = NULL,
+		$tabIndex = NULL
+	) {
+		$this->consolidateCfg($cfg, func_get_args(), func_num_args());
 		parent::__construct($cfg);
 		static::$templates = (object) self::$templates;
 	}
@@ -125,7 +180,6 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField {
 	 * - Translate label text if necessary.
 	 * - Translate value text if necessary.
 	 * - Set up tab-index if necessary.
-	 * - Add supporting javascript file.
 	 * @return void
 	 */
 	public function PreDispatch () {
@@ -133,8 +187,5 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField {
 		if ($this->translate && $this->value)
 			$this->value = $this->form->Translate($this->value);
 		$this->preDispatchTabIndex();
-		$this->form->AddJsSupportFile(
-			$this->jsSupportingFile, $this->jsClassName, [$this->name]
-		);
 	}
 }
