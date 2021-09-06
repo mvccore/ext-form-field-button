6dcb110dbb2f6b4af6113e302c413e1e3e1fcba4
Tom Flídr <tomflidr@gmail.com>
2021-09-06 14:00:22 +0200

    `SafeString` validator refactoring after new view escaping.

--- src/MvcCore/Ext/Forms/Fields/Button.php
+++ src/MvcCore/Ext/Forms/Fields/Button.php
@@ -31,7 +31,7 @@ implements	\MvcCore\Ext\Forms\Fields\IVisibleField {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.1.7';
+	const VERSION = '5.1.8';
 
 	/**
 	 * Possible values: `button` and in extended classes `reset` and `submit`.
